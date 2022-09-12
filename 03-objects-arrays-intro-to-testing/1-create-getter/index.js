5fd59ceae445f0e8b5f69b4abd59e815ec880fca
Alexey Kadantsev <fruktsky@gmail.com>
2022-09-12 14:38:42 +0300

    03 Решена задача: createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const arrayPath = path.split(".");
 
+  return function (obj) {
+    let result = obj;
+
+    for (const keyPath of arrayPath) {
+      if (result) result = result[keyPath];
+    }
+
+    return result;
+  };
 }
